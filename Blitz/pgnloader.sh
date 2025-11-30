5f6ede6d457ac16a601a2381da89bcaf1aa852c1
AlieeLinux <bazartroy531@gmail.com>
2025-11-30 17:20:01 +0800

    chess tours

--- Blitz/pgnloader.sh
+++ Blitz/pgnloader.sh
@@ -1,7 +1,7 @@
 rm /tmp/bayesian.pgn
 find . -iname "*pgn*" -exec cat {} >> /tmp/bayesian.pgn \;
-printf "readpgn /tmp/bayesian.pgn\nelo\nmm\nratings\n" | wl-copy
+printf "readpgn /tmp/bayesian.pgn\nelo\nmm\n" | wl-copy
 
-echo "click CTRL + SHIFT + v"
+echo "click CTRL + SHIFT + v and enter ratings"
 
 ./bayeselo
